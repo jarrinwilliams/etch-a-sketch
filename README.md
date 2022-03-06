21d43ef06375733ac66ec05b2b3c72fe17f1b504
Jarrin Williams <jarrin.williams@yahoo.com>
2022-03-06 09:58:31 -0600

    finished etch-a-sketch

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ black background, with a simple container with buttons and the grid on it, butto
 
 function - fill the grid with as many square divs as the user inputs, when user mouse hovers over the square div, it changes colors to a grey
 -grid container is 700x700 so 16x16 is gonna need 4.375rem
+64x64 is 1.09375rem
+-a way to remove my grid divs
+for (let i = sketchBlocks.length - 1; i >= 0; i--) {
+        sketchContainer.removeChild(sketchBlocks[i]);
+    }
--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-
-but
+// button that makes the 16 grid
 const sixteenBtn = document.querySelector("._16btn");
 sixteenBtn.addEventListener('click', function(e) {
-    for (let i= 0; i < 256; i++) {
+    deleteGrid();
+    for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         square.classList.add('_16sketchBlock');
         document.querySelector('.sketchContainer').appendChild(square);
 
     }
-    const sketchBlocks = document.querySelectorAll('._16sketchBlock');
+    })
+//button for normal mode which is just gray
+const normalBtn = document.querySelector('.normalBtn');
+normalBtn.addEventListener('click', function(e) {
+    const sketchContainer = document.querySelector('.sketchContainer');
+    const sketchBlocks = sketchContainer.childNodes
     sketchBlocks.forEach((sketchBlock) => {
         sketchBlock.addEventListener('mouseover', function(e) {
             sketchBlock.style.background = '#a9a9a9';
-            sketchBlock.style.borderColor = 'black';
+        })
+})
+})
+//button for rainbow mode where a random color is generated
+const rainbowBtn = document.querySelector('.rainbowBtn');
+rainbowBtn.addEventListener('click', function(e) {
+    const sketchContainer = document.querySelector('.sketchContainer')
+    const sketchBlocks = sketchContainer.childNodes
+    sketchBlocks.forEach((sketchBlock) => {
+        sketchBlock.addEventListener('mouseover', function(e) {
+            sketchBlock.style.background = generateRandomColor();
+        })
     })
 })
-sixteenBtn.disabled = true;
-});
 //clear button
 const clearBtn = document.querySelector('.clearBtn');
 clearBtn.addEventListener('click', function(e) {
@@ -25,14 +39,45 @@ clearBtn.addEventListener('click', function(e) {
     sketchBlocks.forEach((block) => {
         block.style.background = 'white';
     })
-    for (let i = sketchBlocks.length - 1; i >= 0; i--) {
-        sketchContainer.removeChild(sketchBlocks[i]);
-    }
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.disabled = false;
-    })
 }
 );
+// button for 64 grid
+const sixtyFourBtn = document.querySelector('._64btn');
+sixtyFourBtn.addEventListener('click', function(e) {
+    deleteGrid();
+    for (let i = 0; i < 4096; i++) {
+        const square = document.createElement('div');
+        square.classList.add('_64sketchBlock')
+        document.querySelector('.sketchContainer').appendChild(square);
 
+    }
+})
+//makes the grid making buttons delete squares if they are present so they cannot overlap
+function deleteGrid() {
+    const sketchContainer = document.querySelector('.sketchContainer')
+    const sketchBlocks = sketchContainer.childNodes;
+    if (sketchBlocks.length === 0) {
+        return
+    } else {
+        for (let i = sketchBlocks.length - 1; i >= 0; i--) {
+            sketchContainer.removeChild(sketchBlocks[i]);
+        }
+    }
+}
+//function to generate a random color
+function generateRandomColor() {
+    const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+//button for 100x100 grid
+const hundredBtn = document.querySelector("._100btn");
+hundredBtn.addEventListener('click', function(e) {
+    deleteGrid();
+    for (let i = 0; i < 10000; i++) {
+        const square = document.createElement('div');
+        square.classList.add('_100sketchBlock');
+        document.querySelector('.sketchContainer').appendChild(square);
+
+    }
+    })
 
