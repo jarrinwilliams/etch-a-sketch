84eb811ba875e49521068e5e065ae5b96be6b564
Jarrin Williams <jarrin.williams@yahoo.com>
2022-03-07 10:17:02 -0600

    refined buttons and polished them

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 // button that makes the 16 grid
 const sixteenBtn = document.querySelector("._16btn");
 sixteenBtn.addEventListener('click', function(e) {
+    sixteenBtn.style.border = 'solid black .5rem'
     deleteGrid();
     for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         square.classList.add('_16sketchBlock');
         document.querySelector('.sketchContainer').appendChild(square);
-
     }
+    morphGridButton(sixteenBtn);
     })
 //button for normal mode which is just gray
 const normalBtn = document.querySelector('.normalBtn');
 normalBtn.addEventListener('click', function(e) {
+    normalBtn.style.border = 'solid black .5rem';
     const sketchContainer = document.querySelector('.sketchContainer');
     const sketchBlocks = sketchContainer.childNodes
     sketchBlocks.forEach((sketchBlock) => {
         sketchBlock.addEventListener('mouseover', function(e) {
             sketchBlock.style.background = '#a9a9a9';
         })
+    morphModeButton(normalBtn);
 })
 })
 //button for rainbow mode where a random color is generated
 const rainbowBtn = document.querySelector('.rainbowBtn');
 rainbowBtn.addEventListener('click', function(e) {
+    rainbowBtn.style.border = 'solid black .5rem';
     const sketchContainer = document.querySelector('.sketchContainer')
     const sketchBlocks = sketchContainer.childNodes
     sketchBlocks.forEach((sketchBlock) => {
         sketchBlock.addEventListener('mouseover', function(e) {
             sketchBlock.style.background = generateRandomColor();
         })
+    morphModeButton(rainbowBtn);
     })
 })
 //clear button
@@ -44,13 +49,14 @@ clearBtn.addEventListener('click', function(e) {
 // button for 64 grid
 const sixtyFourBtn = document.querySelector('._64btn');
 sixtyFourBtn.addEventListener('click', function(e) {
+    sixtyFourBtn.style.border = 'solid black .5rem';
     deleteGrid();
     for (let i = 0; i < 4096; i++) {
         const square = document.createElement('div');
         square.classList.add('_64sketchBlock')
         document.querySelector('.sketchContainer').appendChild(square);
-
     }
+    morphGridButton(sixtyFourBtn);
 })
 //makes the grid making buttons delete squares if they are present so they cannot overlap
 function deleteGrid() {
@@ -72,12 +78,39 @@ function generateRandomColor() {
 //button for 100x100 grid
 const hundredBtn = document.querySelector("._100btn");
 hundredBtn.addEventListener('click', function(e) {
+    hundredBtn.style.border = 'solid black .5rem'
     deleteGrid();
     for (let i = 0; i < 10000; i++) {
         const square = document.createElement('div');
         square.classList.add('_100sketchBlock');
         document.querySelector('.sketchContainer').appendChild(square);
-
     }
+    morphGridButton(hundredBtn);
     })
+//function to make the button clicked border go away when another button is clicked
+function morphGridButton(gridBtn) {
+    const gridBtns = document.querySelectorAll('.gridBtn')
+    gridBtns.forEach((button) => {
+        if (button === gridBtn) {
+            return
+        } else {
+            button.addEventListener('click', function(e) {
+                gridBtn.style.border = 'solid black .1rem';
+            })
+        }
+    })
+}
+// function to do the same thing as the previous function
+function morphModeButton(modeBtn) {
+    const modeBtns = document.querySelectorAll('.modeBtn')
+    modeBtns.forEach((button) => {
+        if (button === modeBtn) {
+            return
+        } else {
+            button.addEventListener('click', function(e) {
+                modeBtn.style.border = 'solid black .1rem';
+            })
+        }
+    })
+}
 
